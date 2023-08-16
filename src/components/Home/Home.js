05a53f6d475e3a3d65d9095512e4ad5f069ca5e1
Suman Ghosh <90588892+sumansingle@users.noreply.github.com>
2023-08-16 08:35:08 +0530

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,40 @@
-import React,{useState} from 'react'
-import '../Home/Home.css';
-import Photo1 from '../../assets/gangate.jpg';
-import Examples from '../Name/examples';
-import Career from '../Career/Career';
-import Skills from '../Skill/Skill';
-import Resume from '../../Resume/Resume';
-import About from '../About/About'
+import React, { useState } from "react";
+import "../Home/Home.css";
+import Photo1 from "../../assets/gangate.jpg";
+import Examples from "../Name/examples";
+import Career from "../Career/Career";
+import Skills from "../Skill/Skill";
+import Resume from "../../Resume/Resume";
+import About from "../About/About";
+import Works from "../Works/Works";
+import ContactFrom from "../Contact From/ContactFrom";
 
 function Home() {
- 
   return (
     <>
-    <div className='home'>
-<div className='intro'>
-  <About/>
-
-</div>
-<div className='component-carr-skill'> 
-  <div className='career'><Career/></div>
-  <div className='skills'><Skills/></div>
-</div>
-</div>
+      <div className="home">
+        <div className="intro">
+          <About />
+        </div>
+        <div className="component-carr-skill">
+          <div className="career">
+            <Career />
+          </div>
+          <div className="skills">
+            <Skills />
+          </div>
+        </div>
+        <div>
+          <div>
+            <Works />
+          </div>
+          <div>
+            <ContactFrom />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
