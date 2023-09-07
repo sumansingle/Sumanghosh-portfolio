588b38a76bf1573faff4eaa18113acb8b222d0af
Suman Ghosh <90588892+sumansingle@users.noreply.github.com>
2023-09-07 20:49:46 +0530

    Update Resume.js

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Resume.css';
 
 const ResumeComponent = () => {
-  const resumePdfUrl = 'https://drive.google.com/file/d/1XrLrzWBrPnOtMtE6lxH4-H47vbtFGTqK/preview'; // Replace with the actual URL of your resume PDF
+  const resumePdfUrl = 'https://drive.google.com/file/d/1E9s1m4LBA3UrFlno9i3oN-6LqsBo7rvd/view?usp=sharing'; // Replace with the actual URL of your resume PDF
 
   return (
     <div className="resume-container">
